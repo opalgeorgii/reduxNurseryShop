5bead1dceb30049b8e8a8d5b9d5c53787dd05cef
Heorhii Vainer <opalgeorgii@gmail.com>
2026-02-19 17:33:48 +0200

    fixed the slice issue

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,17 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const CartSlice = createSlice({
+const cartSlice = createSlice({
   name: "cart",
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
+  initialState: { items: [] },
   reducers: {
-    addItem: (state, action) => {},
-    removeItem: (state, action) => {},
-    updateQuantity: (state, action) => {},
+    addItem: (state, action) => {
+      const { name, image, cost, description } = action.payload;
+
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, description, quantity: 1 });
+      }
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
+    },
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
 
-export default CartSlice.reducer;
+export default cartSlice.reducer;
